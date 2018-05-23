56b02a97f3ff7601d0d83514e7442dccb1f7b7ea
Bhuvanesh71 <uvanesh11237@gmail.com>
2018-05-23 17:59:29 +0530

    Tags in Test execution

--- src/hivcmstestcase/testexecution.java
+++ src/hivcmstestcase/testexecution.java
@@ -4,6 +4,7 @@ import static org.testng.Assert.assertTrue;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -16,7 +17,7 @@ public class testexecution{
 	
 	@Test
 	public void setup() throws InterruptedException {
-	System.setProperty("webdriver.firefox.dirver","C://Users/Bhuvanesh/Desktop/browserexe/firefox.exe");
+	System.setProperty("webdriver.firefox.dirver","C://Users/Bhuvanesh/Desktop/browserexe/chrome.exe");
     WebDriver driver = new ChromeDriver();
     System.out.println("chrome browser is launched");
     driver.get("https://hiv-cmsdev.adappt.co.uk/user");
@@ -53,9 +54,30 @@ public class testexecution{
     driver.findElement(By.xpath("//body[@spellcheck='false']")).sendKeys("These are the different mechanism to identify the same textbox – by using different attributes such as id, name, className or xpath. Here we have kept just one active & commented the others. For details on different element locators, click here");;
     driver.findElement(By.id("edit-field-icon-und-0-browse-button")).click();
     driver.findElement(By.id("edit-upload-upload")).click();
+    // Tags selecting code 
+    driver.findElement(By.xpath("//*[@id='edit_field_tagging_und_chosen']/ul")).click();
+    try {
+        
+        WebElement autoOptions = driver.findElement(By.xpath("//*[@id='edit_field_tagging_und_chosen']/div/ul/li[33]"));
+        autoOptions.click();
+
+        List<WebElement> optionsToSelect = autoOptions.findElements(By.tagName("News"));
+              int indexToSelect =2;
+          if(indexToSelect<=optionsToSelect.size()) {
+                System.out.println("Trying to select based on index: "+indexToSelect);
+                 optionsToSelect.get(indexToSelect).click();
+              }
+      }     
+      catch (NoSuchElementException e) {
+        System.out.println(e.getStackTrace());
+      }
+      catch (Exception e) {
+        System.out.println(e.getStackTrace());
+      
+      }
     
     
 	}
-	
+	 
 	
 }
